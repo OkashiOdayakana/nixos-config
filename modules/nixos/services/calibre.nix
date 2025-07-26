7c92a0f54a7cff9098cac0cc3ae9c6ef30bbe808
Isla <me@isla.pet>
2025-07-26 01:09:24 -0400

    eh2

---
+++ modules/nixos/services/calibre.nix
@@ -0,0 +1,37 @@
+{
+  flake.modules.nixos.calibre =
+    { ... }:
+    {
+      services = {
+        calibre-server = {
+          enable = true;
+          port = 8012;
+          libraries = [
+            "/Nas-main/calibre"
+          ];
+        };
+        calibre-web = {
+          enable = true;
+          options = {
+            reverseProxyAuth = {
+              enable = true;
+              header = "Remote-User";
+            };
+            calibreLibrary = "/Nas-main/calibre";
+            enableBookConversion = true;
+            enableBookUploading = true;
+            enableKepubify = true;
+          };
+        };
+
+        caddy.virtualHosts."calibre.okashi-lan.org".extraConfig = ''
+          forward_auth localhost:9091 {
+                  uri /api/authz/forward-auth
+                  copy_headers Remote-User Remote-Groups Remote-Email Remote-Name
+          }
+
+          reverse_proxy localhost:8012
+        '';
+      };
+    };
+}
